1ca9c64305dc7051a3d011dcfe9fcb3340af51f7
Stuart Harrison <pezholio@gmail.com>
2019-07-08 10:19:08 +0100

    Redirect to login on timeout

--- app/controllers/admin/base_admin_controller.rb
+++ app/controllers/admin/base_admin_controller.rb
@@ -1,11 +1,15 @@
 module Admin
   class BaseAdminController < ApplicationController
-    before_action :ensure_authenticated_user
+    before_action :ensure_authenticated_user, :end_expired_sessions, :update_last_seen_at
 
     private
 
     def ensure_authenticated_user
       redirect_to admin_sign_in_path unless signed_in?
+    end
+
+    def timeout_path
+      admin_sign_in_path
     end
   end
 end
